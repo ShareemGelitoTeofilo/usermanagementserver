c55383cf503d5c8be4003e25e5c0305db070ba1a
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-16 11:23:20 +0800

    Added annotations for persistence

--- src/main/java/usermanagement/friend/Friend.java
+++ src/main/java/usermanagement/friend/Friend.java
@@ -1,11 +1,23 @@
 package usermanagement.friend;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Getter
 @Setter
+@EqualsAndHashCode(of = "id")
 public class Friend {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String name;
     private String email;
     private String address;
