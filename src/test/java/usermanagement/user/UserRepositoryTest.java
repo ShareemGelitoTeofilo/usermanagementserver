d2313a2bc2ceeb129bf7bedc0be8853fedf4dcc5
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-16 11:25:20 +0800

    Added repository tests for saving, finding user by username and password, and finding by username of User

--- src/test/java/usermanagement/user/UserRepositoryTest.java
+++ src/test/java/usermanagement/user/UserRepositoryTest.java
@@ -11,10 +11,29 @@ public class UserRepositoryTest extends DemoApplicationTest {
     UserRepository userRepository;
 
     @Test
-    public void findUser(){
-        User user = userRepository.findUser("shareemgteofilo@yahoo.com", "123");
+    public void saveUser(){
+        User user = UserFactory.create(
+                "Shareem", 21,
+                "ademgelitoteofilo@yahoo.com",
+                "Kalibo, Aklan",
+                "123"
+                );
+        user.setFriends(null);
+        assertNotNull(userRepository.save(user));
+    }
+
+    @Test
+    public void findUserByUsernameAndPassword(){
+        String username = "ademgelitoteofilo@yahoo.com";
+        String password = "123";
+        User user = userRepository.findByUsernameAndPassword(username, password);
         assertNotNull(user);
     }
 
-
+    @Test
+    public void findUserByName(){
+        String username = "shareemgelitoteofilo@yahoo.com";
+        User user = userRepository.findByUsername(username);
+        assertNotNull(user);
+    }
 }
