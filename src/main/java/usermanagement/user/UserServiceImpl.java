ec756352429e481afe49ddf726f6fde3edcaa503
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-16 11:27:47 +0800

    Refactored login to find user by username and password

--- src/main/java/usermanagement/user/UserServiceImpl.java
+++ src/main/java/usermanagement/user/UserServiceImpl.java
@@ -1,18 +1,19 @@
 package usermanagement.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    UserRepository userRepository = new UserRepository();
+    @Autowired
+    UserRepository userRepository;
 
     @Override
     public User login(String username, String password) throws Exception {
-        User user = userRepository.findUser(username, password);
+        User user = userRepository.findByUsernameAndPassword(username, password);
         if(user == null){
             throw new Exception("User not found");
         }
@@ -22,5 +23,14 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> findAllUsers() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public User signUpUser(User user) throws Exception {
+        User existingUserWithSameUsername = userRepository.findByUsername(user.getUsername());
+        if(existingUserWithSameUsername != null){
+            throw new Exception("Username already exist");
+        }
+        return userRepository.save(user);
     }
 }
