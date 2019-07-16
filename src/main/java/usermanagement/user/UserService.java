3e7fb70d4c9572cec189585e17f5e777b778255d
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-16 11:27:08 +0800

    Added an abstract method for signing up user

--- src/main/java/usermanagement/user/UserService.java
+++ src/main/java/usermanagement/user/UserService.java
@@ -5,4 +5,5 @@ import java.util.List;
 public interface UserService {
     User login(String username, String password) throws Exception;
     List<User> findAllUsers();
+    User signUpUser(User user) throws Exception;
 }
