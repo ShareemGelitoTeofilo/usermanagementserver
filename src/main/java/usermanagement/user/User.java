c08d99c030b5c2e13d239ba2a450dd23df16da20
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-16 11:23:33 +0800

    Added annotations for persistence

--- src/main/java/usermanagement/user/User.java
+++ src/main/java/usermanagement/user/User.java
@@ -1,19 +1,32 @@
 package usermanagement.user;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import usermanagement.friend.Friend;
+
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
 @Getter
 @Setter
+@EqualsAndHashCode
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String name;
     private int age;
     private String username;
     private String address;
     private String password;
+
+    public User() {
+    }
+
+    @OneToMany
     private List<Friend> friends;
 
     public User(String name, int age, String username, String address, String password, List<Friend> friends) {
