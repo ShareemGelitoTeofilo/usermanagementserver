cdc1ba7fac16742d52bcc91fce06184dd7238a17
shareem <shareemgelitoteofilo@yahoo.com>
2019-07-16 11:28:11 +0800

    Added a service test for signing up a user

--- src/test/java/usermanagement/user/UserServiceTest.java
+++ src/test/java/usermanagement/user/UserServiceTest.java
@@ -1,5 +1,26 @@
 package usermanagement.user;
 
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.assertNotNull;
+
 public class UserServiceTest extends DemoApplicationTest {
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    public void signUpUser() throws Exception {
+        User user = UserFactory.create(
+                "Retrofit", 21,
+                "retrofit@yahoo.com",
+                "Mars",
+                "123"
+        );
+        user.setFriends(null);
+        user = userService.signUpUser(user);
+        assertNotNull(user);
+    }
 
 }
